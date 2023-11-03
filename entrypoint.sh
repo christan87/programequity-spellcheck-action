a99a3b2fdbe0ebe108f897e6a0656ec395297499
Christan Price <48760919+christan87@users.noreply.github.com>
2023-11-03 13:30:37 +0000

    entrypoint.sh created

---
+++ entrypoint.sh
@@ -0,0 +1,11 @@
+#!/bin/sh
+
+# This script is the entrypoint for the Docker container
+
+# Run cspell with any arguments passed to the script
+# The "$@" is a special variable that holds all command-line arguments
+# The output is redirected to spellcheck-results.txt
+cspell "$@" > spellcheck-results.txt
+
+# Run the Node.js script named index.js
+node index.js
