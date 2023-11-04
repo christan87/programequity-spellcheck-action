6e282eb6398b20647cd60f6c84c0da63ecfd19e7
Christan Price <48760919+christan87@users.noreply.github.com>
2023-11-04 12:34:11 +0000

    test

--- entrypoint.sh
+++ entrypoint.sh
@@ -5,7 +5,7 @@
 # Run cspell with any arguments passed to the script
 # The "$@" is a special variable that holds all command-line arguments
 # The output is redirected to spellcheck-results.txt
-cspell "$@" > /workdir/spellcheck-results.txt
+cspell "$@" > spellcheck-results.txt
 
 # Run the Node.js script named index.js
 node index.js
